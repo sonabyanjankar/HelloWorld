2e0d017fec752f3d06f9782506def34af5ff3e43
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-23 12:21:55 -0600

    New commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+    public static void main(String[] args) {
+
+        String name="Sona Byanjankar";
+
+        System.out.println("Hello . This is " + name + ".");
+
+    }
+}
